fb3cb2baf923401f05c0ca084e2d70b20e40c77b
zhengzh <865851945@qq.com>
2019-09-25 18:48:10 +0800

    添加了一个user实体类

--- src/test/java/com/baizhi/entity/User.java
+++ src/test/java/com/baizhi/entity/User.java
@@ -4,5 +4,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("hehe");
         System.out.println("hehe");
+        System.out.println("jj");
     }
 }
