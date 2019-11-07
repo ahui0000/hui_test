c2a306eb00f05f305404725769ea49b5d0125942
zhengzh <865851945@qq.com>
2019-11-07 14:15:41 +0800

    主分支11月修改

--- src/main/java/com/baizhi/HuiTestApplication.java
+++ src/main/java/com/baizhi/HuiTestApplication.java
@@ -3,11 +3,14 @@ package com.baizhi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * update  by   zhengzhaohui  2019-11-7
+ */
 @SpringBootApplication
 public class HuiTestApplication {
 
     public static void main(String[] args) {
+        //haha
         SpringApplication.run(HuiTestApplication.class, args);
     }
-
 }
